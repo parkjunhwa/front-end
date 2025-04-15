dda7e4f9c44225680e31fcde7eb7ab6ed934acc9
Park Junhwa <junhwa94@ubcare.co.kr>
2025-04-15 12:52:16 +0900

    삭제된 파일 수정

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,40 +1,49 @@
 <script lang="ts" setup>
-import { useConfigStore } from '@core/stores/config'
-import { AppContentLayoutNav } from '@layouts/enums'
-import { switchToVerticalNavOnLtOverlayNavBreakpoint } from '@layouts/utils'
+import { useConfigStore } from "@core/stores/config";
+import { AppContentLayoutNav } from "@layouts/enums";
+import { switchToVerticalNavOnLtOverlayNavBreakpoint } from "@layouts/utils";
 
-const DefaultLayoutWithHorizontalNav = defineAsyncComponent(() => import('./components/DefaultLayoutWithHorizontalNav.vue'))
-const DefaultLayoutWithVerticalNav = defineAsyncComponent(() => import('./components/DefaultLayoutWithVerticalNav.vue'))
+const DefaultLayoutWithVerticalNav = defineAsyncComponent(
+  () => import("./components/DefaultLayoutWithVerticalNav.vue")
+);
 
-const configStore = useConfigStore()
+const configStore = useConfigStore();
 
 // ℹ️ This will switch to vertical nav when define breakpoint is reached when in horizontal nav layout
 // Remove below composable usage if you are not using horizontal nav layout in your app
-switchToVerticalNavOnLtOverlayNavBreakpoint()
+switchToVerticalNavOnLtOverlayNavBreakpoint();
 
-const { layoutAttrs, injectSkinClasses } = useSkins()
+const { layoutAttrs, injectSkinClasses } = useSkins();
 
-injectSkinClasses()
+injectSkinClasses();
 
 // SECTION: Loading Indicator
-const isFallbackStateActive = ref(false)
-const refLoadingIndicator = ref<any>(null)
+const isFallbackStateActive = ref(false);
+const refLoadingIndicator = ref<any>(null);
 
 // watching if the fallback state is active and the refLoadingIndicator component is available
-watch([isFallbackStateActive, refLoadingIndicator], () => {
-  if (isFallbackStateActive.value && refLoadingIndicator.value)
-    refLoadingIndicator.value.fallbackHandle()
+watch(
+  [isFallbackStateActive, refLoadingIndicator],
+  () => {
+    if (isFallbackStateActive.value && refLoadingIndicator.value)
+      refLoadingIndicator.value.fallbackHandle();
 
-  if (!isFallbackStateActive.value && refLoadingIndicator.value)
-    refLoadingIndicator.value.resolveHandle()
-}, { immediate: true })
+    if (!isFallbackStateActive.value && refLoadingIndicator.value)
+      refLoadingIndicator.value.resolveHandle();
+  },
+  { immediate: true }
+);
 // !SECTION
 </script>
 
 <template>
   <Component
     v-bind="layoutAttrs"
-    :is="configStore.appContentLayoutNav === AppContentLayoutNav.Vertical ? DefaultLayoutWithVerticalNav : DefaultLayoutWithHorizontalNav"
+    :is="
+      configStore.appContentLayoutNav === AppContentLayoutNav.Vertical
+        ? DefaultLayoutWithVerticalNav
+        : DefaultLayoutWithHorizontalNav
+    "
   >
     <AppLoadingIndicator ref="refLoadingIndicator" />
 
