3a65524289e2c7dda3ff92ffc8bbf83395a4ac57
Park Junhwa <junhwa94@ubcare.co.kr>
2025-04-16 16:11:47 +0900

    불필요한 파일 삭제4

--- src/views/demos/components/alert/DemoAlertBasic.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="demo-space-y">
-    <VAlert color="primary"> 안녕하세요! 오늘의 알림입니다. </VAlert>
-    <VAlert title="얼럿 제목" type="error" closable text="상세한 설명" />
-  </div>
-</template>
