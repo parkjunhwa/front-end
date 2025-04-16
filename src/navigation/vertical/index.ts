1915896e7877c0b68d5b4e7cf07dfdc4c11f45e8
Park Junhwa <junhwa94@ubcare.co.kr>
2025-04-16 16:10:52 +0900

    불필요한 파일 삭제3

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -1,6 +1,5 @@
 import type { VerticalNavItems } from '@layouts/types'
 import menu_main from './menu_main'
 import menu_mr from './menu_mr'
-import menu_src from './menu_src'
 import menu_wos from './menu_wos'
-export default [...menu_main, ...menu_mr, ...menu_wos, ...menu_src] as VerticalNavItems
+export default [...menu_main, ...menu_mr, ...menu_wos] as VerticalNavItems
